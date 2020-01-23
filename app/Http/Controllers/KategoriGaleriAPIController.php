8a2b00b6681af7f349b0b5001310fad6d51369ce
rahmawati18 <rahma.rrqwe@gmail.com>
2020-01-23 22:29:18 +0800

    belajar api

---
+++ app/Http/Controllers/KategoriGaleriAPIController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\KategoriGaleri;
+use Illuminate\Http\Request;
+
+class KategoriGaleriAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kategoriGaleris=KategoriGaleri::all();
+
+        return $kategoriGaleris;
+        
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input=$request->all();
+
+        $kategorGaleri=KategoriGaleri::create($input);
+
+        return $kategoriGaleri;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $kategoriGaleri=KategoriGaleri::find($id);
+
+        return $kategoriGaleri;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
